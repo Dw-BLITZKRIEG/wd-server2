7828d321962ee52dffec1341a3fda2f4e860b8a1
Glitch (hello-express) <none>
2021-03-29 08:36:08 +0000

    ⛽️👡 Checkpoint ./server.js:28824481/17

--- server.js
+++ server.js
@@ -4640,7 +4640,7 @@ var maintainloop = (() => {
                     let o = new Entity(room.random());
                     o.color = 17;
                     o.define(Class.bot);
-                    o.define(Class.wierd_closer);
+                    o.define(Class.basic);
                     o.name += ran.chooseBotName();
                     o.refreshBodyAttributes();
                     o.color = 17;
