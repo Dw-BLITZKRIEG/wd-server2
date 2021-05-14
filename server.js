c44ab6f9f41b1927ff0f0139d19d8ae2ae155267
Glitch (idiot-2) <none>
2021-05-14 20:25:16 +0000

    💷🗃 Checkpoint ./config.json:28824481/18 ./server.js:28824481/106 ./lib/definitions.js:28824481/2202

--- server.js
+++ server.js
@@ -68,7 +68,6 @@ const room = {
     room.findType('nest');
     room.findType('norm');
     room.findType('bas1');
-    room.findType('bad5');
     room.findType('bas2');
     room.findType('bas3');
     room.findType('bas4');
@@ -2312,7 +2311,6 @@ class Entity {
                 (this.team !== -1 && room.isIn('bas1', loc)) ||
                 (this.team !== -2 && room.isIn('bas2', loc)) ||
                 (this.team !== -3 && room.isIn('bas3', loc)) ||
-                (this.team !== -3 && room.isIn('bad5', loc)) ||
                 (this.team !== -4 && room.isIn('bas4', loc))
             ) { this.kill(); }
         }
@@ -2528,7 +2526,7 @@ function restart3hour() {
           loc,
           -0,
           ran.choose(
-            [Class.arenacloser, Class.arenacloser, Class.arenacloser],
+            [Class.arenacloser2, Class.arenacloser2, Class.arenacloser2],
             1
           )
         );
