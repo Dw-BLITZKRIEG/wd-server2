9e9358ea40a9415e7cc3e0aa51a4fbb6965c35e1
Glitch (idiot-2) <none>
2021-05-15 08:54:59 +0000

    🎭🎭 Checkpoint ./lib/definitions.js:28824481/46 ./server.js:28824481/1414

--- server.js
+++ server.js
@@ -1456,12 +1456,18 @@ var bringToLife = (() => {
         if (my.settings.attentionCraver && !faucet.main && my.range) {
             my.range -= 1;
         }
-        // Invisibility
-          if (my.invisible[1]) {
-		  	    my.alpha = Math.max(0.01, my.alpha - my.invisible[1]);
-		  	    if (!(my.velocity.x * my.velocity.x + my.velocity.y * my.velocity.y < 0.15 * 0.15) || my.damageRecieved)
-		  	      	my.alpha = Math.min(1, my.alpha + my.invisible[0]);
-		    } else my.alpha = 1;
+    // Invisibility
+    if (my.invisible[1]) {
+      my.alpha = Math.max(0.01, my.alpha - my.invisible[1]);
+      if (
+        !(
+          my.velocity.x * my.velocity.x + my.velocity.y * my.velocity.y <
+          0.15 * 0.15
+        ) ||
+        my.damageRecieved
+      )
+        my.alpha = Math.min(1, my.alpha + my.invisible[0]);
+    } else my.alpha = 1;
         // So we start with my master's thoughts and then we filter them down through our control stack
         my.controllers.forEach(AI => {
             let a = AI.think(b);
