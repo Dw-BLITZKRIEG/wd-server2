bb2d5622fb460682de062be4575fa085953b1338
Glitch (idiot-2) <none>
2021-07-02 12:05:56 +0000

    🎯🏃 Checkpoint ./server.js:28824481/1168

--- server.js
+++ server.js
@@ -5053,7 +5053,27 @@ var maintainloop = (() => {
                     o.name += ran.chooseBotName();
                     o.refreshBodyAttributes();
                                   
+                    o.color = 12;
+                    o.team = -3;
+          
+                    bots.push(o)
+                    
+                }
+           if (bots.length < c.BOTS) {
+                    let o = new Entity(room.random());
                     o.color = 17;
+                    o.define(Class.bot);
+                       let arrayOfClasses = [Class.spike, Class.stream, Class.overseer, Class.overlord, Class.weirdspike, Class.fighter, Class.minitrap, Class.knight, Class.spread_script1,
+                                        Class.machinegunner, Class.penta, Class.octo, Class.baby_fac, Class.jr_closer, Class.tri, Class.flank, Class.mach2,
+                                        Class.hybrid, Class.anni, Class.destroy, Class.booster, Class.mega_anni, Class.ninja, Class.RAIL, Class.grower ]                  
+                      let newClass = arrayOfClasses[Math.floor(Math.random() * arrayOfClasses.length)];
+                    o.define(newClass);
+                
+                    o.name += ran.chooseBotName();
+                    o.refreshBodyAttributes();
+                                  
+                    o.color = 10;
+                    o.team = -1;
                   
           
                     bots.push(o)
